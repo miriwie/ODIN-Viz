d9e68c925c478d02c190e49510356c95eadc8e62
j0w <j0w@spline.inf.fu-berlin.de>
2022-02-03 16:25:57 +0100

    add layered chart

--- scripts/linked_charts.js
+++ scripts/linked_charts.js
@@ -11,7 +11,7 @@
           "value": "#b91f13"
         },
         "opacity": {
-          "condition": {"value": 1, "selection": "selector028"},
+          "condition": {"value": 1, "selection": "selector031"},
           "value": 0.2
         },
         "tooltip": [
@@ -24,14 +24,12 @@
         "y": {"type": "quantitative", "field": "Coverage_SC"}
       },
       "height": 650,
-      "selection": {
-        "selector028": {"type": "single", "on": "mouseover", "encodings": ["y"]}
-      },
+      "selection": {"selector031": {"type": "single", "on": "mouseover"}},
       "transform": [{"filter": "(datum.Data_categories === 'All Categories')"}],
       "width": 650
     },
     {
-      "data": {"name": "data-558f7815abfabf1a138a8963876c8c1e"},
+      "data": {"name": "data-08639e0bab20040f849c4ac1f9db48a3"},
       "mark": "bar",
       "encoding": {
         "color": {
@@ -40,21 +38,19 @@
           "scale": {"range": ["#b91f13", "#4682b4"]}
         },
         "opacity": {
-          "condition": {"value": 0.7, "selection": "selector027"},
+          "condition": {"value": 0.7, "selection": "selector031"},
           "value": 0.2
         },
         "tooltip": [
           {"type": "nominal", "field": "Country"},
-          {"type": "quantitative", "field": "Coverage_SC"},
+          {"type": "quantitative", "field": "Score"},
           {"type": "nominal", "field": "Type"}
         ],
         "x": {"type": "nominal", "field": "Country"},
-        "y": {"type": "quantitative", "field": "Coverage_SC", "stack": null}
+        "y": {"type": "quantitative", "field": "Score", "stack": null}
       },
       "height": 650,
-      "selection": {
-        "selector027": {"type": "single", "on": "mouseover", "encodings": ["y"]}
-      },
+      "selection": {"selector031": {"type": "single", "on": "mouseover"}},
       "width": 650
     }
   ],
@@ -16208,383 +16204,383 @@
         "Deviation_norm": -1
       }
     ],
-    "data-558f7815abfabf1a138a8963876c8c1e": [
+    "data-08639e0bab20040f849c4ac1f9db48a3": [
       {
         "Country_Code": "CYP",
         "Country": "Cyprus",
         "Overall": 62.3,
-        "Coverage_SC": 74.1,
+        "Score": 74.1,
         "Type": "Openness"
       },
       {
         "Country_Code": "BGR",
         "Country": "Bulgaria",
         "Overall": 69.9,
-        "Coverage_SC": 73.2,
+        "Score": 73.2,
         "Type": "Openness"
       },
       {
         "Country_Code": "HUN",
         "Country": "Hungary",
         "Overall": 65.3,
-        "Coverage_SC": 67.1,
+        "Score": 67.1,
         "Type": "Openness"
       },
       {
         "Country_Code": "POL",
         "Country": "Poland",
         "Overall": 85.3,
-        "Coverage_SC": 88.8,
+        "Score": 88.8,
         "Type": "Openness"
       },
       {
         "Country_Code": "ROU",
         "Country": "Romania",
         "Overall": 66.2,
-        "Coverage_SC": 73.2,
+        "Score": 73.2,
         "Type": "Openness"
       },
       {
         "Country_Code": "SVK",
         "Country": "Slovakia",
         "Overall": 78.2,
-        "Coverage_SC": 88.5,
+        "Score": 88.5,
         "Type": "Openness"
       },
       {
         "Country_Code": "DNK",
         "Country": "Denmark",
         "Overall": 84.4,
-        "Coverage_SC": 93,
+        "Score": 93,
         "Type": "Openness"
       },
       {
         "Country_Code": "EST",
         "Country": "Estonia",
         "Overall": 70.2,
-        "Coverage_SC": 75.5,
+        "Score": 75.5,
         "Type": "Openness"
       },
       {
         "Country_Code": "FIN",
         "Country": "Finland",
         "Overall": 84.8,
-        "Coverage_SC": 87.1,
+        "Score": 87.1,
         "Type": "Openness"
       },
       {
         "Country_Code": "IRL",
         "Country": "Ireland",
         "Overall": 77.1,
-        "Coverage_SC": 90.4,
+        "Score": 90.4,
         "Type": "Openness"
       },
       {
         "Country_Code": "LVA",
         "Country": "Latvia",
         "Overall": 58.2,
-        "Coverage_SC": 63.6,
+        "Score": 63.6,
         "Type": "Openness"
       },
       {
         "Country_Code": "LTU",
         "Country": "Lithuania",
         "Overall": 73.8,
-        "Coverage_SC": 77,
+        "Score": 77,
         "Type": "Openness"
       },
       {
         "Country_Code": "SWE",
         "Country": "Sweden",
         "Overall": 83.9,
-        "Coverage_SC": 89.3,
+        "Score": 89.3,
         "Type": "Openness"
       },
       {
         "Country_Code": "HRV",
         "Country": "Croatia",
         "Overall": 63.8,
-        "Coverage_SC": 68,
+        "Score": 68,
         "Type": "Openness"
       },
       {
         "Country_Code": "GRC",
         "Country": "Greece",
         "Overall": 60.1,
-        "Coverage_SC": 64.6,
+        "Score": 64.6,
         "Type": "Openness"
       },
       {
         "Country_Code": "ITA",
         "Country": "Italy",
         "Overall": 65.9,
-        "Coverage_SC": 76.5,
+        "Score": 76.5,
         "Type": "Openness"
       },
       {
         "Country_Code": "MLT",
         "Country": "Malta",
         "Overall": 47.2,
-        "Coverage_SC": 49.1,
+        "Score": 49.1,
         "Type": "Openness"
       },
       {
         "Country_Code": "PRT",
         "Country": "Portugal",
         "Overall": 66.5,
-        "Coverage_SC": 75.7,
+        "Score": 75.7,
         "Type": "Openness"
       },
       {
         "Country_Code": "SVN",
         "Country": "Slovenia",
         "Overall": 79.9,
-        "Coverage_SC": 87.7,
+        "Score": 87.7,
         "Type": "Openness"
       },
       {
         "Country_Code": "ESP",
         "Country": "Spain",
         "Overall": 71.3,
-        "Coverage_SC": 73.7,
+        "Score": 73.7,
         "Type": "Openness"
       },
       {
         "Country_Code": "AUT",
         "Country": "Austria",
         "Overall": 68.7,
-        "Coverage_SC": 76.1,
+        "Score": 76.1,
         "Type": "Openness"
       },
       {
         "Country_Code": "BEL",
         "Country": "Belgium",
         "Overall": 45.5,
-        "Coverage_SC": 46.6,
+        "Score": 46.6,
         "Type": "Openness"
       },
       {
         "Country_Code": "FRA",
         "Country": "France",
         "Overall": 62,
-        "Coverage_SC": 66.3,
+        "Score": 66.3,
         "Type": "Openness"
       },
       {
         "Country_Code": "DEU",
         "Country": "Germany",
         "Overall": 77.3,
-        "Coverage_SC": 86,
+        "Score": 86,
         "Type": "Openness"
       },
       {
         "Country_Code": "NLD",
         "Country": "Netherlands",
         "Overall": 81.5,
-        "Coverage_SC": 91.3,
+        "Score": 91.3,
         "Type": "Openness"
       },
       {
         "Country_Code": "LUX",
         "Country": "Luxembourg",
         "Overall": 65.1,
-        "Coverage_SC": 80.9,
+        "Score": 80.9,
         "Type": "Openness"
       },
       {
         "Country_Code": "CZE",
         "Country": "Czech Republic",
         "Overall": 69.8,
-        "Coverage_SC": 71.1,
+        "Score": 71.1,
         "Type": "Coverage"
       },
       {
         "Country_Code": "CYP",
         "Country": "Cyprus",
         "Overall": 62.3,
-        "Coverage_SC": 48.4,
+        "Score": 48.4,
         "Type": "Coverage"
       },
       {
         "Country_Code": "BGR",
         "Country": "Bulgaria",
         "Overall": 69.9,
-        "Coverage_SC": 65.9,
+        "Score": 65.9,
         "Type": "Coverage"
       },
       {
         "Country_Code": "HUN",
         "Country": "Hungary",
         "Overall": 65.3,
-        "Coverage_SC": 63.1,
+        "Score": 63.1,
         "Type": "Coverage"
       },
       {
         "Country_Code": "POL",
         "Country": "Poland",
         "Overall": 85.3,
-        "Coverage_SC": 81.2,
+        "Score": 81.2,
         "Type": "Coverage"
       },
       {
         "Country_Code": "ROU",
         "Country": "Romania",
         "Overall": 66.2,
-        "Coverage_SC": 57.9,
+        "Score": 57.9,
         "Type": "Coverage"
       },
       {
         "Country_Code": "SVK",
         "Country": "Slovakia",
         "Overall": 78.2,
-        "Coverage_SC": 66.2,
+        "Score": 66.2,
         "Type": "Coverage"
       },
       {
         "Country_Code": "DNK",
         "Country": "Denmark",
         "Overall": 84.4,
-        "Coverage_SC": 74.4,
+        "Score": 74.4,
         "Type": "Coverage"
       },
       {
         "Country_Code": "EST",
         "Country": "Estonia",
         "Overall": 70.2,
-        "Coverage_SC": 64,
+        "Score": 64,
         "Type": "Coverage"
       },
       {
         "Country_Code": "FIN",
         "Country": "Finland",
         "Overall": 84.8,
-        "Coverage_SC": 82.1,
+        "Score": 82.1,
         "Type": "Coverage"
       },
       {
         "Country_Code": "IRL",
         "Country": "Ireland",
         "Overall": 77.1,
-        "Coverage_SC": 61.4,
+        "Score": 61.4,
         "Type": "Coverage"
       },
       {
         "Country_Code": "LVA",
         "Country": "Latvia",
         "Overall": 58.2,
-        "Coverage_SC": 51.8,
+        "Score": 51.8,
         "Type": "Coverage"
       },
       {
         "Country_Code": "LTU",
         "Country": "Lithuania",
         "Overall": 73.8,
-        "Coverage_SC": 70,
+        "Score": 70,
         "Type": "Coverage"
       },
       {
         "Country_Code": "SWE",
         "Country": "Sweden",
         "Overall": 83.9,
-        "Coverage_SC": 77.6,
+        "Score": 77.6,
         "Type": "Coverage"
       },
       {
         "Country_Code": "HRV",
         "Country": "Croatia",
         "Overall": 63.8,
-        "Coverage_SC": 58.9,
+        "Score": 58.9,
         "Type": "Coverage"
       },
       {
         "Country_Code": "GRC",
         "Country": "Greece",
         "Overall": 60.1,
-        "Coverage_SC": 55,
+        "Score": 55,
         "Type": "Coverage"
       },
       {
         "Country_Code": "ITA",
         "Country": "Italy",
         "Overall": 65.9,
-        "Coverage_SC": 53.5,
+        "Score": 53.5,
         "Type": "Coverage"
       },
       {
         "Country_Code": "MLT",
         "Country": "Malta",
         "Overall": 47.2,
-        "Coverage_SC": 44.4,
+        "Score": 44.4,
         "Type": "Coverage"
       },
       {
         "Country_Code": "PRT",
         "Country": "Portugal",
         "Overall": 66.5,
-        "Coverage_SC": 55.8,
+        "Score": 55.8,
         "Type": "Coverage"
       },
       {
         "Country_Code": "SVN",
         "Country": "Slovenia",
         "Overall": 79.9,
-        "Coverage_SC": 70.7,
+        "Score": 70.7,
         "Type": "Coverage"
       },
       {
         "Country_Code": "ESP",
         "Country": "Spain",
         "Overall": 71.3,
-        "Coverage_SC": 68.4,
+        "Score": 68.4,
         "Type": "Coverage"
       },
       {
         "Country_Code": "AUT",
         "Country": "Austria",
         "Overall": 68.7,
-        "Coverage_SC": 60.1,
+        "Score": 60.1,
         "Type": "Coverage"
       },
       {
         "Country_Code": "BEL",
         "Country": "Belgium",
         "Overall": 45.5,
-        "Coverage_SC": 44.3,
+        "Score": 44.3,
         "Type": "Coverage"
       },
       {
         "Country_Code": "FRA",
         "Country": "France",
         "Overall": 62,
-        "Coverage_SC": 57.1,
+        "Score": 57.1,
         "Type": "Coverage"
       },
       {
         "Country_Code": "DEU",
         "Country": "Germany",
         "Overall": 77.3,
-        "Coverage_SC": 67,
+        "Score": 67,
         "Type": "Coverage"
       },
       {
         "Country_Code": "NLD",
         "Country": "Netherlands",
         "Overall": 81.5,
-        "Coverage_SC": 70,
+        "Score": 70,
         "Type": "Coverage"
       },
       {
         "Country_Code": "LUX",
         "Country": "Luxembourg",
         "Overall": 65.1,
-        "Coverage_SC": 46.5,
+        "Score": 46.5,
         "Type": "Coverage"
       },
       {
         "Country_Code": "CZE",
         "Country": "Czech Republic",
         "Overall": 69.8,
-        "Coverage_SC": 68.8,
+        "Score": 68.8,
         "Type": "Openness"
       }
     ]
