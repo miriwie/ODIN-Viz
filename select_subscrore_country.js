5b1bb35ae555c15d9f2b3ba5d3fd82f5c2ac8943
danny Vergazova <daniia.vergazova@>
2022-02-07 10:14:56 +0100

    Subcategories

---
+++ select_subscrore_country.js
@@ -0,0 +1,38 @@
+//*********************************************
+// Function that Shows an HTML element
+//*********************************************
+function showDiv(divID)
+{
+	var div = document.getElementById(divID);
+	div.style.display = ""; //display div
+}
+
+//*********************************************
+// Function that Hides an HTML element
+//*********************************************
+function hideDiv(divID)
+{
+	var div = document.getElementById(divID);
+	div.style.display = "none"; // hide
+}
+//*****************************************************************************
+// Function that Hides all the Div elements in the select menu Value
+//*****************************************************************************
+function hideAllDivs()
+{
+	//Loop through the seclect menu values and hide all
+	var selectMenu = document.getElementById("selectMenu");
+	for (var i=0; i<=selectMenu.options.length -1; i++)
+	{
+		hideDiv(selectMenu.options[i].value);
+	}
+}
+//*********************************************
+// Main function that calls others to toggle divs
+//*********************************************
+function toggle(showID)
+{
+	hideAllDivs(); // Hide all
+	showDiv(showID); // Show the one we asked for
+
+}
